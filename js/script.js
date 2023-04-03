4307f321dd4eafc158367091b8c3d68dee6e112f
SerhiiChynin <sergey.chinin@gmail.com>
2023-04-03 13:38:31 +0300

    start

---
+++ js/script.js
@@ -0,0 +1,22 @@
+console.log('Hello');
+console.log('World!');
+console.log('333');
+console.log(333);
+console.log('Hello' + ' world');
+console.log('Hello ' + 'world');
+console.log('Hello' + ' ' + 'world');
+console.info('hello');
+// alert('Hello');
+document.querySelector('#out').innerHTML = 'Hello world!';
+document.getElementById('out').innerHTML = 'Hello baby!'
+document.getElementById('out').innerHTML = '<b>2023</b>' ;
+document.querySelector('.header').innerHTML = 15;
+document.getElementById('one').innerHTML = 'Hello hahaha'
+document.querySelector('#one').innerHTML = 777;
+
+let a;
+a = document.querySelector('#out');
+a.innerHTML = 'Coca Cola';
+let b = document.querySelector('.header');
+b.innerHTML = 'Today is a big day!';
+
