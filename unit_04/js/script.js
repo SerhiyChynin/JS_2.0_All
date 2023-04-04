5f857901a893cda953d4682b1aeedb88f906b326
SerhiiChynin <sergey.chinin@gmail.com>
2023-04-04 18:10:10 +0300

    text aria

---
+++ unit_04/js/script.js
@@ -0,0 +1,32 @@
+document.querySelector('button').onclick = myFunc;
+
+function myFunc() {
+    console.log(document.querySelector('.one').value);    
+    document.querySelector('button').style.background = document.querySelector('.one').value;
+}
+
+document.querySelector('.one').oninput = () => {
+     console.log(document.querySelector('.one').value); 
+    document.querySelector('span').innerHTML = document.querySelector('.one').value;
+}
+
+
+document.querySelector('#btn-1').onclick = () => {
+    console.log(document.querySelector('#i-2').value); 
+    let checkBox = document.querySelector('#i-2');
+    console.log(checkBox.checked);
+    if (checkBox.checked) {
+        console.log('Working');
+    }
+    else {
+        console.log('not working');
+    }
+}
+
+document.querySelector('#btn-2').onclick = (event) => { 
+    event.preventDefault();
+    // console.log(document.querySelector('#i-3').value); 
+    // document.querySelector('#i-3').value = 'orange';
+    let form = document.querySelector('form');
+    console.log(form.elements.i3.value);
+}
