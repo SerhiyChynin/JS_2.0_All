fff35759bb510d7c7c85d09394253aa67df7debf
SerhiiChynin <sergey.chinin@gmail.com>
2023-04-26 14:19:53 +0300

    fix

--- unit_18/js20_unit_18_template/unit_18/unit_18.js
+++ unit_18/js20_unit_18_template/unit_18/unit_18.js
@@ -54,15 +54,11 @@ document.querySelector('.b-4').onclick = t4;
 // Task 5 ============================================
 /*  На странице созданы 3 p.task-5 c атрибутом data. С помощью forEach переберите их и добавьте событие клик. Напишите функцию t5, которая будет запускаться при клике и добавлять атрибут data элемента, по которому кликнули в массив a5_res.*/
 
-function t5(elem) {
-    // let p = document.querySelectorAll('.task-5');
-    // p.forEach(el => a5_res.push(el.innerHTML))
-        a5_res.push(this.getAttribute('data'));
+function t5() {
+    a5_res.push(this.getAttribute('data'));
     document.querySelector('.out-5').innerHTML = a5_res;
-    // console.log(a5_res);
-    }
-
-document.querySelectorAll('.task-5').forEach(elem => {
+}
+    document.querySelectorAll('.task-5').forEach(elem => {
     elem.onclick = t5;
 });
 
