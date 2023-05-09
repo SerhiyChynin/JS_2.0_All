b5a821727ea5d04f64a12480c0c726b961f09671
SerhiiChynin <sergey.chinin@gmail.com>
2023-05-09 14:46:19 +0300

    recursion

--- unit_29/js/script.js
+++ unit_29/js/script.js
@@ -8,8 +8,35 @@ function f1() {
 
 let b = f1();
 let c = f1();
-console.log(c());
-console.log(b);
-console.log(b());
-console.log(b());
-console.log(b());
+// console.log(c());
+// console.log(c());
+// console.log(c());
+// console.log(c());
+// console.log(b());
+// console.log(b());
+// console.log(b());
+
+let a = 0;
+function rec() {
+    a++;
+    console.log(a);
+    if (a > 10) return a = 0;
+    rec();
+}
+if (a === 0) {
+    
+    rec()
+}
+console.log(a);
+
+let n = 0;
+function move() {
+    
+    document.querySelector('.out').style.left = n + 'px';
+    n = n + 10;
+    if (n >= 500) return n;
+    setTimeout(move, 100)
+    
+}
+document.querySelector('button').onclick = move;
+
